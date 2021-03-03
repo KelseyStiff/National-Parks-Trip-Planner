508969c6443aea9525004320390c1271f57a51b0
Andrea <apratt1013@gmail.com>
2021-03-03 16:21:31 -0600

    removed API key

--- unsplash_api.py
+++ unsplash_api.py
@@ -1,7 +1,10 @@
 import requests
+import os
 
 national_park = input('What park do you want images for? ')
 
-random_park_image = requests.get(f'https://api.unsplash.com/photos/random?query="{national_park}";client_id=3a31301fc8edce2497d0e0dd001ec1bfbed7a207b9207dd2ccc5c87baade2b12').json()
+key = os.environ('UNSPLASH_KEY')
+
+random_park_image = requests.get(f'https://api.unsplash.com/photos/random?query="{national_park}";client_id={key}').json()
     
 image_url = random_park_image['links']['download']
