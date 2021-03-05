7ee8903afe109d55e4567b5e6b326ac30d99d6eb
Andrea <apratt1013@gmail.com>
2021-03-05 15:08:34 -0600

    constraints for database fields

--- model.py
+++ model.py
@@ -10,16 +10,16 @@ class BaseModel(Model):
         database = db
 
 
-class Park(BaseModel):
-    park_name = CharField(null=False, unique=True) # The user shouldn't be able to add the same park twice
-    park_city = CharField(null=False)
-    park_state = CharField(null=False)
-    park_description = CharField(null=False)
-    latitude = DecimalField(null=False)
-    longitude = DecimalField(null=False)
-    image_1 = CharField()
-    image_2 = CharField()
-    image_3 = CharField()
+class Park(BaseModel): # The user shouldn't be able to add the same park twice 
+    park_name = CharField(null=False, unique=True, constraints=[Check('length(park_name) > 0'), Check('length(park_name) <= 30')]) 
+    park_city = CharField(null=False, constraints=[Check('length(park_city) > 0'), Check('length(park_city) <= 30')])
+    park_state = CharField(null=False, constraints=[Check('length(park_state) > 0'), Check('length(park_state) <= 2')]) # State code
+    park_description = CharField(null=False, constraints=[Check('length(park_description) > 0'), Check('length(park_description) <= 200')])
+    latitude = DecimalField(null=False, constraints=[Check('length(latitude) > 0'), Check('length(latitude) <= 30')])
+    longitude = DecimalField(null=False, constraints=[Check('length(longitude) > 0'), Check('length(longitude) <= 30')])
+    image_1 = CharField(constraints=[Check('length(image_1) <= 100')])
+    image_2 = CharField(constraints=[Check('length(image_2) <= 100')])
+    image_3 = CharField(constraints=[Check('length(image_3) <= 100')])
 
 
 
