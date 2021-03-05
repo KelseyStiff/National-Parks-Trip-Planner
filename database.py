390f87fe76241bbbbc4ac79610bc57b9db4830df
Andrea <apratt1013@gmail.com>
2021-03-05 14:12:31 -0600

    add function that return the coordinates of all saved parks as a dictionary

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ import model
 import peewee
 
 
-def save_park(name, city, state, description, latitude, longitude, image1, image2, image3):  
+"""Accepts all values for a park object and saves it to the database. All parameters are required except images."""
+def save_park(name, city, state, description, latitude, longitude, image1=None, image2=None, image3=None):  
     park = Park(
         park_name = name, park_city = city, park_state = state, 
         park_description = description, latitude = latitude, 
@@ -17,6 +18,7 @@ def get_park_by_name(park_name):
     return Park.get(park_name = park_name)
 
 
+"""Returns all saved park objects"""
 def get_all_parks():
     parks = Park.select()
     return parks
@@ -33,11 +35,24 @@ def get_park_latitude_by_name(park_name):
     return park.latitude
 
 
+
 def get_park_longitude_by_name(park_name):
     park = get_park_by_name(park_name)
     return park.longitude
     
 
+"""Returns a dictionary containing the float coordinates of all saved parks"""
+def get_all_parks_coordinates(): #
+    parks = get_all_parks()
+    coordinates = {}
+    for p in parks:
+        coordinates.update({float(p.latitude) : float(p.longitude)}) 
+    return coordinates
 
 
 
+# save_park('Yellowstone National Park', 'Middlanoware', 'Wyoming', 'Beautiful scenic park.', 1234567.89, 9876543.21, 'sldksjdlf', 'sdlkjsdfl', 'sldkjsldkfd')
+# save_park('Yosemite', 'Somewhere', 'Nevada', 'Cool national park.', 1234567.890, 09876543.21, 'sldksjdl', 'sdlkjsdf', 'sldkjsldkf')
+# save_park('Random Park', 'A City', 'Some State', 'This is the park description.', 555555.55, 66666.66, 'asdfsdfs', 'sdfdfsdsfdaf', 'sdfsdfadfasdf')
+
+print(get_all_parks_coordinates())
