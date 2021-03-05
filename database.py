717b6d59aa2abed1280bd11915dae9cf28872242
Andrea <apratt1013@gmail.com>
2021-03-05 15:43:34 -0600

    some error handling added

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from model import Park
 import model
 import peewee
+from peewee import IntegrityError
 
 
 """
@@ -8,17 +9,20 @@ Accepts all values for a park object and saves it to the database. All parameter
 Some park names may not pull up enough pictures.
 """
 def save_park(name, city, state, description, latitude, longitude, image1=None, image2=None, image3=None):  
-    park = Park(
-        park_name = name, park_city = city, park_state = state, 
-        park_description = description, latitude = latitude, 
-        longitude = longitude, image_1 = image1, image_2 = image2, 
-        image_3 = image3
-        )
-    park.save()
+    try:
+        park = Park(
+            park_name = name, park_city = city, park_state = state, 
+            park_description = description, latitude = latitude, 
+            longitude = longitude, image_1 = image1, image_2 = image2, 
+            image_3 = image3
+            )
+        park.save()
+    except IntegrityError:
+        return "There was an issue while trying to save this park."
 
 
-def get_park_by_name(park_name):
-    return Park.get(park_name = park_name)
+def get_park_by_name(park_name): # Returns none if park not in database
+    return Park.get_or_none(park_name = park_name)
 
 
 """Returns all saved park objects"""
@@ -27,10 +31,12 @@ def get_all_parks():
     return parks
 
 
-def get_park_city(park_name):
+def get_park_city(park_name): # Returns None if the park's not found
     park = get_park_by_name(park_name)
-    city = park.park_city
-    return city
+    if park != None:
+        return park.park_city
+    else:
+        return None
 
 
 def get_park_latitude_by_name(park_name):
@@ -44,7 +50,7 @@ def get_park_longitude_by_name(park_name):
     
 
 """Returns a dictionary containing the float coordinates of all saved parks"""
-def get_all_parks_coordinates(): #
+def get_all_parks_coordinates(): 
     parks = get_all_parks()
     coordinates = {}
     for p in parks:
@@ -57,4 +63,4 @@ def get_all_parks_coordinates(): #
 # save_park('Yosemite', 'Somewhere', 'Nevada', 'Cool national park.', 1234567.890, 09876543.21, 'sldksjdl', 'sdlkjsdf', 'sldkjsldkf')
 # save_park('Random Park', 'A City', 'Some State', 'This is the park description.', 555555.55, 66666.66, 'asdfsdfs', 'sdfdfsdsfdaf', 'sdfsdfadfasdf')
 
-print(get_all_parks_coordinates())
+print(get_park_by_name('Yosemite'))
