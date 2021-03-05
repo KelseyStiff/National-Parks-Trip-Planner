17ddd30c41c416632418c1da4a89e859735e3832
Andrea <apratt1013@gmail.com>
2021-03-05 12:19:36 -0600

    environment variable retrieval fixed

--- unsplash_api.py
+++ unsplash_api.py
@@ -1,10 +1,14 @@
 import requests
 import os
 
+# TODO add exception handling for connection error, and key error (some searches may not return an image)
+
 national_park = input('What park do you want images for? ')
 
-key = os.environ('UNSPLASH_KEY')
+key = os.environ.get('UNSPLASH_KEY')
 
 random_park_image = requests.get(f'https://api.unsplash.com/photos/random?query="{national_park}";client_id={key}').json()
     
 image_url = random_park_image['links']['download']
+
+print(image_url)
