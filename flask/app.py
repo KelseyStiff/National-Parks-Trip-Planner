ed145ce4064b5d2bb03e63390bcea2d3a821cdb6
kelseystiff <kelsey>
2021-03-05 21:05:58 -0600

    created map

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,16 @@
 import flask
 from flask import request, url_for, render_template, redirect
+import sys
+sys.path.insert(1, 'project_4/states_and_months')
 
 app = flask.Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
     # content for app goes here 
-    return render_template('index.html')
+    # key = os.environ.get('MAPBOX_KEY')
+    key = 'pk.eyJ1Ijoia2l0dHlzdGlmZiIsImEiOiJjazZ2aDF3ZzcwMXNxM2hvMmJiZTlvaTI5In0.oEO-8s7LpbrCHJatQnXVKg'
+    return render_template('index.html', key=key)
 
 if __name__ == '__main__':
     app.run(debug=True)
