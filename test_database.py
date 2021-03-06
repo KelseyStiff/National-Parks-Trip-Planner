7e8738b72149fb3bbdec641bfa17ff915e5fcca2
Andrea <apratt1013@gmail.com>
2021-03-05 18:48:40 -0600

    rearranged functions

--- test_database.py
+++ test_database.py
@@ -26,6 +26,15 @@ class TestParksDB(TestCase):
         database.save_park('Random Park', 'A City', 'SS', 'This is the park description.', 555555.55, 66666.66, 'image url 1', 'image url 2', 'image url 3')
 
 
+    def test_save_park(self):
+        database.save_park('Park Name', 'Park City', 'PS', 'Park Description', 65656556.0, 55555.000, 'image', 'image', 'image')
+        park = Park.get_or_none(park_name = 'Park Name')
+        self.assertIsNotNone(park)
+        
+        park = Park.get_or_none(park_name = 'NAME THAT DOES NOT EXIST') 
+        self.assertIsNone(park)
+
+
     def test_get_park_by_name(self):
         self.add_sample_data()
         park = database.get_park_by_name('Yosemite')
@@ -36,13 +45,7 @@ class TestParksDB(TestCase):
         self.assertEqual(actual, expected)
 
     
-    def test_save_park(self):
-        database.save_park('Park Name', 'Park City', 'PS', 'Park Description', 65656556.0, 55555.000, 'image', 'image', 'image')
-        park = Park.get_or_none(park_name = 'Park Name')
-        self.assertIsNotNone(park)
-        
-        park = Park.get_or_none(park_name = 'NAME THAT DOES NOT EXIST') 
-        self.assertIsNone(park)
+ 
 
 
 if __name__ == '__main__':
