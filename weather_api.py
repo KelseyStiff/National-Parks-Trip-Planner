24c3d58d13f889c6cb0da835f1e0f7dd39a63e9d
Andrea <apratt1013@gmail.com>
2021-03-06 22:43:24 -0600

    api key environment variable

--- weather_api.py
+++ weather_api.py
@@ -2,7 +2,7 @@ import requests
 import os
 from pprint import pprint
 
-key = 'd5iUahogk60F0E2GYZ2OK37j9LpL0hSm'
+key = os.environ.get('CLIMATE_KEY')
 url = 'https://api.meteostat.net/v2/point/climate?lat=44&lon=-93&alt=58'
 
 headers={'x-api-key': key}
