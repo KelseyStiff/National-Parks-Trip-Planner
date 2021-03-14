eeaafd8fa91515c783b2707d5ec5c92f8fe761db
Mohammad Sargazi <sargazi1377@gmail.com>
2021-03-13 20:48:40 -0600

    api key is now hidden and more info available

--- national_parks_api.py
+++ national_parks_api.py
@@ -1,22 +1,26 @@
 import requests
 from prettyprinter import pprint
-urls = ['https://developer.nps.gov/api/v1/parks?&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb',
-'https://developer.nps.gov/api/v1/parks?start=101&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb',
-'https://developer.nps.gov/api/v1/parks?start=51&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb', 
-'https://developer.nps.gov/api/v1/parks?start=151&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb', 
-'https://developer.nps.gov/api/v1/parks?start=202&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb', 
-'https://developer.nps.gov/api/v1/parks?start=252&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb', 
-'https://developer.nps.gov/api/v1/parks?start=302&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb', 
-'https://developer.nps.gov/api/v1/parks?start=352&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb',
-'https://developer.nps.gov/api/v1/parks?start=402&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb',
-'https://developer.nps.gov/api/v1/parks?start=452&api_key=8KEJWdmy0u98yA2aqe9uLH5A2SVkNzBzhjez2xMb',
+import os
+key = os.environ.get('NATIONAL_PARKS_KEY')
+querry = {'api_key': key}
+
+urls = ['https://developer.nps.gov/api/v1/parks?&',
+'https://developer.nps.gov/api/v1/parks?start=101&',
+'https://developer.nps.gov/api/v1/parks?start=51&', 
+'https://developer.nps.gov/api/v1/parks?start=151&', 
+'https://developer.nps.gov/api/v1/parks?start=202&', 
+'https://developer.nps.gov/api/v1/parks?start=252&', 
+'https://developer.nps.gov/api/v1/parks?start=302&', 
+'https://developer.nps.gov/api/v1/parks?start=352&',
+'https://developer.nps.gov/api/v1/parks?start=402&',
+'https://developer.nps.gov/api/v1/parks?start=452&'
 ]
 
 
 try:
      User_states = input('Enter:')
      for l in urls: # Since the api has a limit of 50 parks, we have to have multiple apis to run after each other. This line loops through all the 400 data in the api
-        data= requests.get(l).json()
+        data= requests.get(l, params=querry).json()
         #pprint(data)
         list_of_parks = data['data']
        
@@ -31,8 +35,12 @@ try:
                 description = x['description']
                 address = x['addresses'][1]
                 city = address['city']
+                weather_info = x['weatherInfo']
+                postalcode = address['postalCode']
+                line1 = address['line1']
+                state_code= address['stateCode']
 
-                #print(f'{name} : latitude: {latitude}, longitude: {longitude}') 
-                print(name)
+                print(f'Name: {name}, address: {line1}, {city}, {state_code},{postalcode} \n Weather info : {weather_info}')
+            
 except: 
     print('API not responding')      
