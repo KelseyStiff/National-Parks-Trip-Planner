129bf916a8ddb9302080787f1b8a102f43c36281
kelseystiff <kelsey>
2021-03-17 15:48:58 -0500

    hid API Key

--- flask/app.py
+++ flask/app.py
@@ -9,8 +9,7 @@ app = flask.Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    # key = os.environ.get('MAPBOX_KEY')
-    key = 'pk.eyJ1Ijoia2l0dHlzdGlmZiIsImEiOiJjazZ2aDF3ZzcwMXNxM2hvMmJiZTlvaTI5In0.oEO-8s7LpbrCHJatQnXVKg'
+    key = os.environ.get('MAPBOX_KEY')
     return render_template('index.html', key=key)
 
 if __name__ == '__main__':
