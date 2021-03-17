83076a4412ac601c83cb052b7839f84f9cef4395
Andrea <apratt1013@gmail.com>
2021-03-17 18:24:49 -0500

    climate_api module now accepts a trip parameter and adds climate data to it before returning it

--- national_parks_api.py
+++ national_parks_api.py
@@ -19,14 +19,14 @@ f'https://developer.nps.gov/api/v1/parks?start=451&api_key={key}',
 ]
 
 """All interaction with this module should be done through this function"""
-def get_park_data(state):
+def get_park_data(state, month):
     state_code = _get_state_code(state.upper())
     list_of_parks = _call_park_api()
-    parks = _create_trip_object_list(list_of_parks, state_code)
+    parks = _create_trip_object_list(list_of_parks, state_code, month)
     return parks
 
 
-def _create_trip_object_list(list_of_parks, state_code):   
+def _create_trip_object_list(list_of_parks, state_code, month):   
     parks_in_state = []
     try:
         # Since the api has a limit of 50 parks, we have to have multiple urls to run after each other. This line loops through all the 400 data in the api   
@@ -43,7 +43,7 @@ def _create_trip_object_list(list_of_parks, state_code):
                 latitude = park['latitude']
                 longitude = park['longitude']     
 
-                trip = Trip(park_name=park_name, park_city=park_city, park_state=park_state, 
+                trip = Trip(month=month, park_name=park_name, park_city=park_city, park_state=park_state, 
                             park_description=park_description, latitude=latitude, longitude=longitude)
                         
                 parks_in_state.append(trip)
@@ -73,6 +73,6 @@ def _call_park_api():
         return "Error getting data. Try checking your internet connection."
 
 
-parks = get_park_data('UTah')
+parks = get_park_data('UTah', 'November')
 for park in parks:
         print(park)
--- trip.py
+++ trip.py
@@ -1,7 +1,8 @@
 
 class Trip:
-    def __init__(self, park_name, park_city, park_state, park_description, latitude, longitude, 
+    def __init__(self, month, park_name, park_city, park_state, park_description, latitude, longitude, 
                  image_1=None, image_2=None, image_3=None, precipitation=None, avg_temp=None, max_temp=None, min_temp=None):
+        self.month = month
         self.park_name = park_name
         self.park_city = park_city
         self.park_state = park_state
@@ -15,9 +16,10 @@ class Trip:
         self.avg_temp = avg_temp
         self.max_temp = max_temp
         self.min_temp = min_temp
+        
 
 
     def __str__(self):
-        return f'{self.park_name}\n{self.park_city}\n{self.park_state}\n{self.park_description}\n\
+        return f'{self.month}\n{self.park_name}\n{self.park_city}\n{self.park_state}\n{self.park_description}\n\
 {self.latitude}\n{self.longitude}\n{self.image_1}\n{self.image_2}\n{self.image_3}\n\
-{self.precipitation}\n{self.avg_temp}\n{self.max_temp}\n{self.min_temp}\n\n'
+{self.precipitation}%\n{self.avg_temp}\n{self.max_temp}\n{self.min_temp}\n\n'
