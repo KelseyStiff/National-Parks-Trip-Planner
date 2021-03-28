331fbf63d297bd12b73ea93f2088b7cb17743dc8
kelseystiff <kelsey>
2021-03-28 17:54:08 -0500

    testing

--- tests/test_unsplash_api.py
+++ tests/test_unsplash_api.py
@@ -4,20 +4,16 @@ from unittest import TestCase
 from unittest.mock import patch
 from apis import unsplash_api
 
-
-key =  'MMZ9lcnReyfxYUFX507ZrlD7lokdDdDyFYplI3Ze9FE'
-url = 'https://api.unsplash.com/search/photos'
-
 class TestUnsplashApi(TestCase):
     @patch('unsplash_api._unsplash_api_call')
     def test_api_query(self, mock_query):
-        mock_query = {'query':'yosemite', 'per_page':4, 'client_id':key}
-        example_response = requests.get(url, params=mock_query).json()
-
-        example_query = {'query':'yosemite', 'per_page':4, 'client_id':key}
-        expected_response = requests.get(url, params=example_query).json()
-        
-        self.assertEqual(expected_response, example_response)
+        mock_response = "results": [] "urls": {"small": "test"}
+       #create fake response with fake image urls
+        mock_trip_object_no_pics = {}  #create trip object with no pics
+        trip_object_with_pics = unsplash_api.get_park_image(mock_trip_object_no_pics) #call method to add pics to trip object
+        expected_trip_object_with_pics = {}  #create second trip object that has right pictures
+        #compare return from that method - should have the proper
+        self.assertEqual(expected_trip_object_with_pics, trip_object_with_pics)
 
 if __name__ == '__main__':
     unittest.main()
