245c0da710afba507c460abed669628d178fd34b
kelseystiff <kelsey>
2021-03-28 21:17:34 -0500

    testing unsplash api

--- tests/test_unsplash_api.py
+++ tests/test_unsplash_api.py
@@ -3,15 +3,18 @@ import requests
 from unittest import TestCase
 from unittest.mock import patch
 from apis import unsplash_api
+from unsplash_mock_data import response, trip_w_pic, trip_no_pic
+
+
 
 class TestUnsplashApi(TestCase):
     @patch('unsplash_api._unsplash_api_call')
-    def test_api_query(self, mock_query):
-        mock_response = "results": [] "urls": {"small": "test"}
-       #create fake response with fake image urls
-        mock_trip_object_no_pics = {}  #create trip object with no pics
-        trip_object_with_pics = unsplash_api.get_park_image(mock_trip_object_no_pics) #call method to add pics to trip object
-        expected_trip_object_with_pics = {}  #create second trip object that has right pictures
+    def test_api_query(self, mock_response):
+        m_response = response #create fake response with fake image urls
+        mock_response.side_effect = m_response
+        mock_trip_no_pics = trip_no_pic #create trip object with no pics
+        trip_object_with_pics = unsplash_api.get_park_image(mock_trip_no_pics) #call method to add pics to trip object
+        expected_trip_object_with_pics = trip_w_pic #create second trip object that has right pictures
         #compare return from that method - should have the proper
         self.assertEqual(expected_trip_object_with_pics, trip_object_with_pics)
 
---
+++ tests/unsplash_mock_data.py
@@ -0,0 +1,408 @@
+from database.model import Trip, Park
+
+
+response ={
+"total": 10000,
+"total_pages": 2500,
+"results": [
+{
+"id": "TuKjDq44tQw",
+"created_at": "2019-11-07T14:47:46-05:00",
+"updated_at": "2021-03-28T18:09:07-04:00",
+"promoted_at": None,
+"width": 3024,
+"height": 4032,
+"color": "#8cc0d9",
+"blur_hash": "L+FP{VRQWBWUlCNGj[oLM}oMoffk",
+"description": "Lomonosov (Oranienbaum), Russia. Original file here (link to Dropbox): https://vk.cc/9ZGbpZ",
+"alt_description": "dirt pathway between grass field",
+"urls": {
+"raw": "https://images.unsplash.com/photo-1573155993874-d5d48af862ba?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwxfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1",
+"full": "https://images.unsplash.com/photo-1573155993874-d5d48af862ba?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwxfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=85",
+"regular": "https://images.unsplash.com/photo-1573155993874-d5d48af862ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwxfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=1080",
+"small": "imageurl1",
+"thumb": "https://images.unsplash.com/photo-1573155993874-d5d48af862ba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwxfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=200"
+},
+"links": {
+"self": "https://api.unsplash.com/photos/TuKjDq44tQw",
+"html": "https://unsplash.com/photos/TuKjDq44tQw",
+"download": "https://unsplash.com/photos/TuKjDq44tQw/download",
+"download_location": "https://api.unsplash.com/photos/TuKjDq44tQw/download?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwxfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ"
+},
+"categories": [],
+"likes": 109,
+"liked_by_user": '',
+"current_user_collections": [],
+"sponsorship": None,
+"user": {
+"id": "Nj6yzPSrtjA",
+"updated_at": "2021-03-28T14:34:21-04:00",
+"username": "malyushev",
+"name": "Victor Malyushev",
+"first_name": "Victor",
+"last_name": "Malyushev",
+"twitter_username": "malyushev",
+"portfolio_url": "https://clc.to/malyushev",
+"bio": "Mobile Photography. Shot on iPhone. Enhanced by Snapseed.",
+"location": "Saint Petersburg, Russia",
+"links": {
+"self": "https://api.unsplash.com/users/malyushev",
+"html": "https://unsplash.com/@malyushev",
+"photos": "https://api.unsplash.com/users/malyushev/photos",
+"likes": "https://api.unsplash.com/users/malyushev/likes",
+"portfolio": "https://api.unsplash.com/users/malyushev/portfolio",
+"following": "https://api.unsplash.com/users/malyushev/following",
+"followers": "https://api.unsplash.com/users/malyushev/followers"
+},
+"profile_image": {
+"small": "https://images.unsplash.com/profile-1564991939975-41ffd591b500?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+"medium": "https://images.unsplash.com/profile-1564991939975-41ffd591b500?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+"large": "https://images.unsplash.com/profile-1564991939975-41ffd591b500?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+},
+"instagram_username": "malyushev",
+"total_collections": 3,
+"total_likes": 75,
+"total_photos": 269,
+"accepted_tos": '',
+"for_hire": ''
+},
+"tags": [
+{
+"type": "search",
+"title": "plant"
+},
+{
+"type": "landing_page",
+"title": "grass",
+"source": {
+"ancestry": {
+"type": {
+"slug": "backgrounds",
+"pretty_slug": "Backgrounds"
+},
+"category": {
+"slug": "nature",
+"pretty_slug": "Nature"
+},
+"subcategory": {
+"slug": "grass",
+"pretty_slug": "Grass"
+}
+},
+"title": "Grass Backgrounds",
+"subtitle": "Download free grass background images",
+"description": "Choose from a curated selection of grass backgrounds. Always free on Unsplash.",
+"meta_title": "900+ Grass Background Images: Download HD Backgrounds on Unsplash",
+"meta_description": "Choose from hundreds of free grass backgrounds. Download beautiful, curated free backgrounds on Unsplash.",
+"cover_photo": {
+"id": "YRfaG3JVwkU",
+"created_at": "2019-04-29T18:00:09-04:00",
+"updated_at": "2021-03-26T10:08:25-04:00",
+"promoted_at": "2019-04-30T02:43:11-04:00",
+"width": 4608,
+"height": 3072,
+"color": "#262626",
+"blur_hash": "LMC778Rlayoe~lodV]WD?XayfikA",
+"description": None,
+"alt_description": "green field",
+"urls": {
+"raw": "https://images.unsplash.com/photo-1556575157-75a0d60e4835?ixlib=rb-1.2.1",
+"full": "https://images.unsplash.com/photo-1556575157-75a0d60e4835?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb",
+"regular": "https://images.unsplash.com/photo-1556575157-75a0d60e4835?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
+"small": "imageurl2",
+"thumb": "https://images.unsplash.com/photo-1556575157-75a0d60e4835?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max"
+},
+"links": {
+"self": "https://api.unsplash.com/photos/YRfaG3JVwkU",
+"html": "https://unsplash.com/photos/YRfaG3JVwkU",
+"download": "https://unsplash.com/photos/YRfaG3JVwkU/download",
+"download_location": "https://api.unsplash.com/photos/YRfaG3JVwkU/download"
+},
+"categories": [],
+"likes": 232,
+"liked_by_user": '',
+"current_user_collections": [],
+"sponsorship": None,
+"user": {
+"id": "_TBj9DtXXy8",
+"updated_at": "2021-03-26T10:27:51-04:00",
+"username": "elcuervo",
+"name": "Bruno Aguirre",
+"first_name": "Bruno",
+"last_name": "Aguirre",
+"twitter_username": "cuerbot",
+"portfolio_url": "https://elcuervo.net",
+"bio": None,
+"location": "The nest",
+"links": {
+"self": "https://api.unsplash.com/users/elcuervo",
+"html": "https://unsplash.com/@elcuervo",
+"photos": "https://api.unsplash.com/users/elcuervo/photos",
+"likes": "https://api.unsplash.com/users/elcuervo/likes",
+"portfolio": "https://api.unsplash.com/users/elcuervo/portfolio",
+"following": "https://api.unsplash.com/users/elcuervo/following",
+"followers": "https://api.unsplash.com/users/elcuervo/followers"
+},
+"profile_image": {
+"small": "https://images.unsplash.com/profile-1478636822683-74af79580009?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+"medium": "https://images.unsplash.com/profile-1478636822683-74af79580009?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+"large": "https://images.unsplash.com/profile-1478636822683-74af79580009?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+},
+"instagram_username": None,
+"total_collections": 1,
+"total_likes": 101,
+"total_photos": 156,
+"accepted_tos": None,
+"for_hire": '',
+}
+}
+}
+},
+{
+"type": "search",
+"title": "outdoors"
+}
+]
+},
+{
+"id": "SBiVq9eWEtQ",
+"created_at": "2018-02-22T15:32:34-05:00",
+"updated_at": "2021-03-28T22:02:59-04:00",
+"promoted_at": None,
+"width": 5760,
+"height": 3840,
+"color": "#40400c",
+"blur_hash": "LMFO-mRPVxrw0NnUjZox-nkRRoWC",
+"description": None,
+"alt_description": "photo of empty park during daytime",
+"urls": {
+"raw": "https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwyfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1",
+"full": "https://images.unsplash.com/photo-1519331379826-f10be5486c6f?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwyfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=85",
+"regular": "https://images.unsplash.com/photo-1519331379826-f10be5486c6f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwyfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=1080",
+"small": "https://images.unsplash.com/photo-1519331379826-f10be5486c6f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwyfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=400",
+"thumb": "https://images.unsplash.com/photo-1519331379826-f10be5486c6f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwyfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=200"
+},
+"links": {
+"self": "https://api.unsplash.com/photos/SBiVq9eWEtQ",
+"html": "https://unsplash.com/photos/SBiVq9eWEtQ",
+"download": "https://unsplash.com/photos/SBiVq9eWEtQ/download",
+"download_location": "https://api.unsplash.com/photos/SBiVq9eWEtQ/download?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwyfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ"
+},
+"categories": [],
+"likes": 222,
+"liked_by_user": '',
+"current_user_collections": [],
+"sponsorship": None,
+"user": {
+"id": "EfS61utlXME",
+"updated_at": "2021-03-28T17:59:26-04:00",
+"username": "mbenna",
+"name": "Mike Benna",
+"first_name": "Mike",
+"last_name": "Benna",
+"twitter_username": None,
+"portfolio_url": "https://www.linkedin.com/in/mikebenna/",
+"bio": None,
+"location": "Vancouver, Canada",
+"links": {
+"self": "https://api.unsplash.com/users/mbenna",
+"html": "https://unsplash.com/@mbenna",
+"photos": "https://api.unsplash.com/users/mbenna/photos",
+"likes": "https://api.unsplash.com/users/mbenna/likes",
+"portfolio": "https://api.unsplash.com/users/mbenna/portfolio",
+"following": "https://api.unsplash.com/users/mbenna/following",
+"followers": "https://api.unsplash.com/users/mbenna/followers"
+},
+"profile_image": {
+"small": "https://images.unsplash.com/profile-1519658624086-19053f2ab674?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+"medium": "https://images.unsplash.com/profile-1519658624086-19053f2ab674?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+"large": "https://images.unsplash.com/profile-1519658624086-19053f2ab674?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+},
+"instagram_username": None,
+"total_collections": 0,
+"total_likes": 0,
+"total_photos": 131,
+"accepted_tos": None,
+"for_hire": ''
+},
+"tags": []
+},
+{
+"id": "W0lNN2WvaEA",
+"created_at": "2017-09-07T15:03:15-04:00",
+"updated_at": "2021-03-28T01:17:56-04:00",
+"promoted_at": None,
+"width": 2988,
+"height": 5312,
+"color": "#595959",
+"blur_hash": "LFEe-_D%S1T1?dI9xuo~BtjDxaxu",
+"description": "At The Big Park In Sarajevo",
+"alt_description": "brown trees",
+"urls": {
+"raw": "https://images.unsplash.com/photo-1504810935423-dbbe9a698963?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwzfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1",
+"full": "https://images.unsplash.com/photo-1504810935423-dbbe9a698963?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwzfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=85",
+"regular": "https://images.unsplash.com/photo-1504810935423-dbbe9a698963?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwzfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=1080",
+"small": "imageurl3",
+"thumb": "https://images.unsplash.com/photo-1504810935423-dbbe9a698963?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwzfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=200"
+},
+"links": {
+"self": "https://api.unsplash.com/photos/W0lNN2WvaEA",
+"html": "https://unsplash.com/photos/W0lNN2WvaEA",
+"download": "https://unsplash.com/photos/W0lNN2WvaEA/download",
+"download_location": "https://api.unsplash.com/photos/W0lNN2WvaEA/download?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHwzfHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ"
+},
+"categories": [],
+"likes": 116,
+"liked_by_user": '',
+"current_user_collections": [],
+"sponsorship": None,
+"user": {
+"id": "Nm5XLeaeEhk",
+"updated_at": "2021-03-25T11:34:20-04:00",
+"username": "azganmjeshtri",
+"name": "AZGAN MjESHTRI",
+"first_name": "AZGAN",
+"last_name": "MjESHTRI",
+"twitter_username": None,
+"portfolio_url": None,
+"bio": None,
+"location": None,
+"links": {
+"self": "https://api.unsplash.com/users/azganmjeshtri",
+"html": "https://unsplash.com/@azganmjeshtri",
+"photos": "https://api.unsplash.com/users/azganmjeshtri/photos",
+"likes": "https://api.unsplash.com/users/azganmjeshtri/likes",
+"portfolio": "https://api.unsplash.com/users/azganmjeshtri/portfolio",
+"following": "https://api.unsplash.com/users/azganmjeshtri/following",
+"followers": "https://api.unsplash.com/users/azganmjeshtri/followers"
+},
+"profile_image": {
+"small": "https://images.unsplash.com/profile-1505242058418-714ad9f5c43a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+"medium": "https://images.unsplash.com/profile-1505242058418-714ad9f5c43a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+"large": "https://images.unsplash.com/profile-1505242058418-714ad9f5c43a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+},
+"instagram_username": "azgan_mjeshtri",
+"total_collections": 1,
+"total_likes": 7,
+"total_photos": 24,
+"accepted_tos": '',
+"for_hire": ''
+},
+"tags": [
+{
+"type": "search",
+"title": "plant"
+},
+{
+"type": "search",
+"title": "flora"
+},
+{
+"type": "search",
+"title": "ivy"
+}
+]
+},
+{
+"id": "P9gkfbaxMTU",
+"created_at": "2019-09-14T13:12:51-04:00",
+"updated_at": "2021-03-28T16:08:44-04:00",
+"promoted_at": None,
+"width": 5713,
+"height": 3809,
+"color": "#0c260c",
+"blur_hash": "LWF~J}xt9boc0PfhayNIxroexYoL",
+"description": None,
+"alt_description": "sun shining on tree",
+"urls": {
+"raw": "https://images.unsplash.com/photo-1568480289356-5a75d0fd47fc?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHw0fHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1",
+"full": "https://images.unsplash.com/photo-1568480289356-5a75d0fd47fc?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHw0fHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=85",
+"regular": "https://images.unsplash.com/photo-1568480289356-5a75d0fd47fc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHw0fHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=1080",
+"small": "imageurl4",
+"thumb": "https://images.unsplash.com/photo-1568480289356-5a75d0fd47fc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHw0fHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ&ixlib=rb-1.2.1&q=80&w=200"
+},
+"links": {
+"self": "https://api.unsplash.com/photos/P9gkfbaxMTU",
+"html": "https://unsplash.com/photos/P9gkfbaxMTU",
+"download": "https://unsplash.com/photos/P9gkfbaxMTU/download",
+"download_location": "https://api.unsplash.com/photos/P9gkfbaxMTU/download?ixid=MnwyMTE3OTV8MHwxfHNlYXJjaHw0fHxwYXJrfGVufDB8fHx8MTYxNjk4MzU0NQ"
+},
+"categories": [],
+"likes": 51,
+"liked_by_user": '',
+"current_user_collections": [],
+"sponsorship": None,
+"user": {
+"id": "cHYya4LwotM",
+"updated_at": "2021-03-28T18:45:09-04:00",
+"username": "gmalhotra",
+"name": "Gayatri Malhotra",
+"first_name": "Gayatri",
+"last_name": "Malhotra",
+"twitter_username": "gayatrim16",
+"portfolio_url": "http://gayatrimalhotra.com",
+"bio": "Globetrotter",
+"location": "Washington, DC",
+"links": {
+"self": "https://api.unsplash.com/users/gmalhotra",
+"html": "https://unsplash.com/@gmalhotra",
+"photos": "https://api.unsplash.com/users/gmalhotra/photos",
+"likes": "https://api.unsplash.com/users/gmalhotra/likes",
+"portfolio": "https://api.unsplash.com/users/gmalhotra/portfolio",
+"following": "https://api.unsplash.com/users/gmalhotra/following",
+"followers": "https://api.unsplash.com/users/gmalhotra/followers"
+},
+"profile_image": {
+"small": "https://images.unsplash.com/profile-1608514617921-235a62acbd71image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+"medium": "https://images.unsplash.com/profile-1608514617921-235a62acbd71image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+"large": "https://images.unsplash.com/profile-1608514617921-235a62acbd71image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+},
+"instagram_username": "gayatrim",
+"total_collections": 6,
+"total_likes": 44,
+"total_photos": 292,
+"accepted_tos": '',
+"for_hire": '',
+},
+"tags": []
+}
+]
+}
+
+park = Park(
+  park_id = '2',
+  park_name = "Yosemite",
+  city = "Yosemite Valley",
+  state = 'self.park.park_state',
+  description = 'a park',
+  latitude = 100.00,
+  longitude = 100.00,
+)
+
+trip_no_pic = Trip (
+  month = 'january',
+  park = park,
+  latitude = 100.00,
+  longitude = 100.00,
+  image_1 = None,
+  image_2 = None,
+  image_3 = None,
+  image_4 = None,
+  precipiation = 1.0,
+  avg_temp = 50,
+  max_temp = 70,
+  min_temp = 30,
+)
+
+trip_w_pic = Trip (
+  month = 'january',
+  park = park,
+  image_1 = 'imageurl1',
+  image_2 = 'imageurl2',
+  image_3 = 'imageurl3',
+  image_4 = 'imageurl4',
+  precipiation = 1.0,
+  avg_temp = 50,
+  max_temp = 70,
+  min_temp = 30,
+)
